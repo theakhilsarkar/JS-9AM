3e6e4c1f683a13925fd5e93363219707834affb0
theakhilsarkar <akhilsodvadiya7@gmail.com>
2026-08-27 10:00:00 +0530

    ...

---
+++ day 16/script.js
@@ -0,0 +1,45 @@
+// DOM - Document Object Model
+// we can create and modify html elements and their style by using DOM.
+
+// document = index.html -> script src. script.js
+
+// html -> object -> key value ->
+
+// document.body
+// document.body.style.backgroundColor = "red";
+
+// const headings = document.getElementsByTagName
+
+// headings[0].textContent = "Changed Page";
+
+// console.log(headings[0].textContent);
+// console.log(headings[1]);
+// more than value - array, object
+
+// CSS Selectors
+// tag, class, id
+
+// const heading1 = document.getElementById("heading-1");
+// heading1.textContent = "New HEADING HERE....";
+
+// const btn = document.getElementById("btn");
+
+// btn.textContent = "Register";
+// btn.addEventListener("click", () => {
+//   console.log("button pressed !!");
+// });
+
+// interaction
+// JS WE CAN GIVE INTERACTIVITY TO THE WEBPAGE/
+
+// const btn = document.getElementById("btn");
+
+// btn.textContent = "Change";
+
+// btn.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "green";
+// });
+
+
+// button -> press -> body bg change
+// every click -> random color
