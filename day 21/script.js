5f4b9ac00bc0ed3c657eeab92ce8546cf0efc9ec
theakhilsarkar <akhilsodvadiya7@gmail.com>
2026-09-08 10:01:08 +0530

    ...

---
+++ day 21/script.js
@@ -0,0 +1,11 @@
+// create a note making app, where user can create upto 150 characters note, user can also add more than one notes, and display all notes in grid view and boxes color should be different for each notes.
+
+const input_note = document.getElementById("input-note");
+const input_btn = document.getElementById("input-btn");
+const notes_box = document.getElementById("notes-box");
+
+const handleCreateNote = () => {
+  // logic...
+};
+
+input_btn.addEventListener("click", handleCreateNote);
