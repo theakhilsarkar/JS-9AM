806c1aca79ea4eb39414daf9a61ac5466144d7da
theakhilsarkar <akhilsodvadiya7@gmail.com>
2026-09-09 09:55:10 +0530

    ....

--- day 21/script.js
+++ day 21/script.js
@@ -2,10 +2,32 @@
 
 const input_note = document.getElementById("input-note");
 const input_btn = document.getElementById("input-btn");
+
 const notes_box = document.getElementById("notes-box");
 
 const handleCreateNote = () => {
-  // logic...
+  const value = input_note.value; // value = Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur
+
+  // random color generation
+  const color = "#" + Math.floor(Math.random() * 16581375).toString(16);
+
+  const div = document.createElement("div"); // <div></div>
+  div.className = "px-3 py-3 rounded"; // <div class="bg-warning px-3 py-3 rounded"></div>
+  div.id = "note";
+  div.style.backgroundColor = color;
+  div.textContent = value; //<div class="bg-warning px-3 py-3 rounded"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur</div>
+
+  notes_box.appendChild(div); // we are insert new div into notes box
+
+  input_note.value = ""; // we are empty value of input box
 };
 
 input_btn.addEventListener("click", handleCreateNote);
+
+// CRUD = 
+// Create, add, insert
+// Read = display
+// Update, modify, edit
+// Delete, remove
+
+// create, display
