7a12a329f8a6d89cadb13b046825d3f99b8efed3
theakhilsarkar <akhilsodvadiya7@gmail.com>
2026-09-15 09:58:40 +0530

    ....

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -43,36 +43,36 @@ const questionsList = [
     options: ["Ag", "Fe", "Au", "Pb"],
     answer: 2,
   },
-  {
-    question: "Which country is home to the kangaroo?",
-    options: ["South Africa", "Australia", "Brazil", "New Zealand"],
-    answer: 1,
-  },
-  {
-    question: "How many continents are there on Earth?",
-    options: ["5", "6", "7", "8"],
-    answer: 2,
-  },
-  {
-    question: "What is the hardest natural substance on Earth?",
-    options: ["Diamond", "Quartz", "Granite", "Titanium"],
-    answer: 0,
-  },
-  {
-    question: "Which gas do plants primarily absorb during photosynthesis?",
-    options: ["Oxygen", "Nitrogen", "Hydrogen", "Carbon Dioxide"],
-    answer: 3,
-  },
-  {
-    question: "Who painted the Mona Lisa?",
-    options: [
-      "Vincent van Gogh",
-      "Leonardo da Vinci",
-      "Pablo Picasso",
-      "Claude Monet",
-    ],
-    answer: 1,
-  },
+  // {
+  //   question: "Which country is home to the kangaroo?",
+  //   options: ["South Africa", "Australia", "Brazil", "New Zealand"],
+  //   answer: 1,
+  // },
+  // {
+  //   question: "How many continents are there on Earth?",
+  //   options: ["5", "6", "7", "8"],
+  //   answer: 2,
+  // },
+  // {
+  //   question: "What is the hardest natural substance on Earth?",
+  //   options: ["Diamond", "Quartz", "Granite", "Titanium"],
+  //   answer: 0,
+  // },
+  // {
+  //   question: "Which gas do plants primarily absorb during photosynthesis?",
+  //   options: ["Oxygen", "Nitrogen", "Hydrogen", "Carbon Dioxide"],
+  //   answer: 3,
+  // },
+  // {
+  //   question: "Who painted the Mona Lisa?",
+  //   options: [
+  //     "Vincent van Gogh",
+  //     "Leonardo da Vinci",
+  //     "Pablo Picasso",
+  //     "Claude Monet",
+  //   ],
+  //   answer: 1,
+  // },
 ];
 
 const question_index = document.getElementById("question-index");
@@ -91,10 +91,32 @@ const option_D_btn = document.getElementById("option-d-btn");
 const timer_counter = document.getElementById("timer-count");
 const next_question_btn = document.getElementById("next-question-btn");
 
+const start_quiz_btn = document.getElementById("start-btn");
+const quiz_container = document.getElementById("quiz-container");
+
 let currentQuestionIndex = -1;
-let seconds = 60;
+let timerId = 0;
 
 const displayQuestion = () => {
+  // mark calculation & quiz end on last question
+  if (currentQuestionIndex >= questionsList.length - 1) {
+    alert("Quiz Ended !");
+    return;
+  }
+
+  // timer and next question after timer end.
+  let seconds = 10;
+  timerId = setInterval(() => {
+    timer_counter.textContent = seconds;
+    if (seconds > 0) {
+      seconds--;
+    } else {
+      clearInterval(timerId);
+      displayQuestion();
+    }
+  }, 1000);
+
+  // display question by currentQuestionIndex
   currentQuestionIndex++;
   question_index.textContent = "Q-" + (currentQuestionIndex + 1);
   question.textContent = questionsList[currentQuestionIndex].question;
@@ -105,11 +127,18 @@ const displayQuestion = () => {
 };
 
 // pending
-setInterval(() => {
-  timer_counter.textContent = seconds;
-  seconds--;
-}, 1000);
 
-next_question_btn.addEventListener("click", displayQuestion);
+// next question display on click and previous timer clear and restart
+next_question_btn.addEventListener("click", () => {
+  clearInterval(timerId);
+  displayQuestion();
+});
 
+// to display first question without any click and start timer.
 displayQuestion();
+
+// start quiz btn
+start_quiz_btn.addEventListener("click", () => {
+  quiz_container.className = "container";
+  start_quiz_btn.className = "d-none";
+});
